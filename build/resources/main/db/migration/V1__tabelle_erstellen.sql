43b4b2b35807fef7842091d3287298db1406c852
Dysil100 <silatsamdylan@gmail.com>
2022-02-03 16:09:03 +0100

    -gitignore angefasst

---
+++ build/resources/main/db/migration/V1__tabelle_erstellen.sql
@@ -0,0 +1,51 @@
+create table if not exists users
+(
+    id         serial primary key,
+    first_name varchar(60) unique not null,
+    last_name  varchar(60) unique not null,
+    email      varchar(100)       not null,
+    password   varchar(200)       not null,
+    locked     boolean,
+    enabled    boolean,
+    role       varchar(40)        not null
+);
+
+create table if not exists archiv
+(
+    id         serial primary key,
+    first_name varchar(60) unique not null,
+    last_name  varchar(60) unique not null,
+    email      varchar(100)       not null,
+    password   varchar(200)       not null
+);
+
+create table if not exists confirmation_token
+(
+    id             serial primary key,
+    token          varchar(200) not null,
+    created_at     varchar(20) not null,
+    expired_at     varchar(20) not null,
+    confirmed_at   varchar(20),
+    username      varchar(100)       not null
+);
+
+create table if not exists ingredient
+(
+    id                    serial primary key,
+    name                  text not null,
+    lysine                decimal,
+    methyonine            decimal,
+    proteine_brute        decimal,
+    energie_metabolisable decimal
+);
+
+create table if not exists standard
+(
+    id                    serial primary key,
+    description           text not null,
+    lysine                decimal,
+    methyonine            decimal,
+    proteine_brute        decimal,
+    energie_metabolisable decimal
+);
+
---
+++ build/resources/main/db/migration/V2__exampleData.sql
@@ -0,0 +1,21 @@
+
+insert into ingredient(name, lysine, methyonine, proteine_brute, energie_metabolisable)
+VALUES ('tourteau de coton', 1.6, .62, 39.0, 1525.0),
+    ('belgofoxs', 0.0, 0.0, 0.0, 0.0),
+    ('belgoporcs', 0.0, 0.0, 0.0, 0.0),
+    ('belgotox', 0.0, 0.0, 0.0, 0.0),
+    ('coquille de mer', 0.0, 0.0, 0.0, 0.0),
+    ('farine de soja', 2.16, 0.49, 34.9, 3239.6),
+    ('son de blé', .65, 0.25, 15.6, 1580.0),
+    ('sulfate de fer', .0, .0, .0, .0),
+    ('tourteau d`arachide', 1.42, 0.25, 43.01, 2268.0),
+    ('tourteau de palmiste', 0.44, 0.28, 14.8, 1927.0),
+    ('mais', .24, .17, 9.0, 3280.0),
+    ('farine de poisson', 4.25, 1.55, 55.2, 3157.0);
+
+insert into standard(description, lysine, methyonine, proteine_brute, energie_metabolisable)
+VALUES ('Porcins: Prédémarrage Porc (de 5 à 10 Jours)', .81, .4, 15.00, 2500.0),
+       ('Porcins: Finition', 1.21, .52, 21.4, 2800.0),
+       ('Porcins: Demarrage ou porcelet sevrés', 1.14, .47, 19.58, 2700.0),
+       ('Porcins: Croissance (30 à 60 Kilos)', .92, .42, 16.68, 2650.0),
+       ('Porcins: Truies Gestantes/ Alliatante et Verra', .81, .4, 15.0, 2500.0);
