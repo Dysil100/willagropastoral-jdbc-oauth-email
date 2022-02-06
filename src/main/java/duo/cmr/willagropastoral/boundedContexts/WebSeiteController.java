afbaa9f15a927f512a13346e2d5abd5dd09cd7e8
Dysil100 <silatsamdylan@gmail.com>
2022-02-06 18:59:41 +0100

    fragment eingesetz

--- src/main/java/duo/cmr/willagropastoral/boundedContexts/WebSeiteController.java
+++ src/main/java/duo/cmr/willagropastoral/boundedContexts/WebSeiteController.java
@@ -1,8 +1,13 @@
-package duo.cmr.willagropastoral.boundedContexts.dasandere.web.controllers;
+package duo.cmr.willagropastoral.boundedContexts;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebSeiteController {
+    @GetMapping("/newindex")
+    public String index(){
+        return "newindex";
+    }
     // TODO: 03.02.22 implement all file related to the Web-Site so almost all may be just getMapping;
 }
